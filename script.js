39b3e9df2c4ff3a0786a0ecb5ab691f9b09bafd9
FitzChiv97 <porter.morgan.97bb@gmail.com>
2025-08-23 10:55:59 +0300

    localStorage, delete buttons added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+renderTodoList();
+//get todoList from localStorage and put in on the page
 
 document
   .querySelector('.js-add-button')
@@ -19,6 +21,8 @@ function addTodo() {
   //get text from a textbox
 
   todoList.push(inputValue);
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  //save input value into todoList and localStorage 
 
   inputElement.value = '';  
   //reset the text in the textbox
@@ -26,14 +30,21 @@ function addTodo() {
   renderTodoList();
 }
 
+
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(todo => {
-    const htmlElement = `<p>${todo}</p>`;
+  todoList.forEach(todo => {    
+    const htmlElement = `
+      <p class="js-task-name" id="${genereateID()}">
+        ${todo} 
+        <button class="js-delete-button">Delete</button>
+      </p>
+    `;
+  
     todoListHTML += htmlElement;
-  //this is a generating HTML technique
   })
+  //generate HTML for each todoList element
 
   document
     .querySelector('.js-todo-list')
@@ -43,4 +54,27 @@ function renderTodoList() {
   rewriting <div>'s value, not adding to it.
   that's why the todoList values aren't duplicated each time.
   */
+
+  const deleteButtons = document.querySelectorAll('.js-delete-button');
+  deleteButtons.forEach(button => button.addEventListener('click', deleteTask));
+  //find & add deleteEvent to all deleteButtons
+}
+
+
+function deleteTask() {
+  let taskName = this.parentElement.innerText;
+  taskName = taskName.slice(0, -7);
+  //get todo taskName from <p> element
+
+  const taskIndex = todoList.indexOf(taskName);
+  todoList.splice(taskIndex, 1);
+  //find taskIndex in todoList and remove task from there
+
+  this.parentElement.remove();
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  //remove <p> element from the page & update localStorage
+}
+
+function genereateID() {
+  return Math.random().toString(16).slice(-5);
 }
